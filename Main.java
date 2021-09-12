b6268dae10016361fe8ca0997d5937843d42534b
Colin McQuinn <89641085+mcquinnca@users.noreply.github.com>
2021-09-12 17:08:03 +0000

    Added loop to count up/down to zero.

--- Main.java
+++ Main.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    System.out.println("Enter a number to count down or up from:");
+    Scanner s = new Scanner(System.in);
+    int count = s.nextInt();
+    System.out.println("\n \n" + count);
+
+    // loop to count up or down to zero
+    while (count != 0) {
+      // function to count down if the number is positive
+      if (count > 0) {
+        count--;
+        System.out.println(count);
+      }
+      // Function to count up the the number is negative
+      else if (count < 0) {
+        count++;
+        System.out.println(count);
+      }
+    }
+    System.out.println("Blast off!");
   }
 }
